e7c5ab8ca7ba4088a091c0991eb0bf9f30f03106
Toke Friis <tokefriis@gmail.com>
2021-11-29 15:41:18 +0100

    inserted simple quiz

--- main.py
+++ main.py
@@ -1,12 +1,80 @@
-import pygame
+def new_game():
 
-(width, height) = (1000, 1000)
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('FIRE spillet')
+    guesses = []
+    correct_guesses = 0
+    question_num = 1
 
-pygame.display.flip()
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    for key in questions:
+        print("-------------------------")
+        print(key)
+        for i in options[question_num-1]:
+            print(i)
+        guess = input("Skriv (A, B, C, or D): ")
+        guess = guess.upper()
+        guesses.append(guess)
+
+        correct_guesses += check_answer(questions.get(key), guess)
+        question_num += 1
+
+    display_score(correct_guesses, guesses)
+
+# -------------------------
+def check_answer(answer, guess):
+
+    if answer == guess:
+        print("CORRECT!")
+        return 1
+    else:
+        print("WRONG!")
+        return 0
+
+# -------------------------
+def display_score(correct_guesses, guesses):
+    print("-------------------------")
+    print("RESULTAT")
+    print("-------------------------")
+
+    print("Svar: ", end="")
+    for i in questions:
+        print(questions.get(i), end=" ")
+    print()
+
+    print("Gæt: ", end="")
+    for i in guesses:
+        print(i, end=" ")
+    print()
+
+    score = int((correct_guesses/len(questions))*100)
+    print("Din score er: "+str(score)+"%")
+
+# -------------------------
+def play_again():
+
+    response = input("Vil du spille igen? (ja eller nej): ")
+    response = response.upper()
+
+    if response.upper() == "JA":
+        return True
+    else:
+        return False
+# -------------------------
+
+
+questions = {
+ "Who created Python?: ": "A",
+ "What year was Python created?: ": "B",
+ "Python is tributed to which comedy group?: ": "C",
+ "Is the Earth round?: ": "A"
+}
+
+options = [["A. Guido van Rossum", "B. Elon Musk", "C. Bill Gates", "D. Mark Zuckerburg"],
+          ["A. 1989", "B. 1991", "C. 2000", "D. 2016"],
+          ["A. Lonely Island", "B. Smosh", "C. Monty Python", "D. SNL"],
+          ["A. True","B. False", "C. sometimes", "D. What's Earth?"]]
+
+new_game()
+
+while play_again():
+    new_game()
+
+print("Byeeeeee!")
