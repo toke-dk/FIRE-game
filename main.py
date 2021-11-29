bf2cc2ff2dfffee96ec958b5d1b1c747be133d14
Toke Friis <tokefriis@gmail.com>
2021-11-29 15:29:15 +0100

    created a window

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 import pygame
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+(width, height) = (1000, 1000)
+screen = pygame.display.set_mode((width, height))
+pygame.display.flip()
+pygame.display.set_caption('FIRE spillet')
+running = True
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
