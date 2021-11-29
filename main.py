4f0a5c6a22b628b8d0b858e922cdf2c31f9268db
Toke Friis <tokefriis@gmail.com>
2021-11-29 15:30:10 +0100

    reformat

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
+
 (width, height) = (1000, 1000)
 screen = pygame.display.set_mode((width, height))
-pygame.display.flip()
 pygame.display.set_caption('FIRE spillet')
+
+pygame.display.flip()
 running = True
 while running:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
