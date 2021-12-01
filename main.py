b4d3a587d1cb42466b9a9c05465bb560226d38ef
Toke Friis <tokefriis@gmail.com>
2021-12-01 19:33:34 +0100

    displays all questions

--- main.py
+++ main.py
@@ -102,19 +102,19 @@ while running:
              "svar": "A"
              },
             {"spørgsmål": "Hvad vil du helst købe?",
-             "valgmuligheder": [{"tekst": "A. Bil", "point": 2, "pris": 100, "inkomststigning": 10},
+             "valgmuligheder": [{"tekst": "A. Bilfd", "point": 2, "pris": 100, "inkomststigning": 10},
                                 {"tekst": "B. Tog", "point": 5, "pris": 12, "inkomststigning": 7},
                                 {"tekst": "C. Hus", "point": 100, "pris": 12, "inkomststigning": 10}],
              "svar": "A"
              },
             {"spørgsmål": "Hvad vil du helst købe?",
-             "valgmuligheder": [{"tekst": "A. Bil", "point": 2, "pris": 100, "inkomststigning": 10},
+             "valgmuligheder": [{"tekst": "A. Biadl", "point": 2, "pris": 100, "inkomststigning": 10},
                                 {"tekst": "B. Tog", "point": 5, "pris": 12, "inkomststigning": 7},
                                 {"tekst": "C. Hus", "point": 100, "pris": 12, "inkomststigning": 10}],
              "svar": "A"
              },
             {"spørgsmål": "Hvad vil du helst købeee?",
-             "valgmuligheder": [{"tekst": "A. Bil", "point": 2, "pris": 100, "inkomststigning": 10},
+             "valgmuligheder": [{"tekst": "A. Bilfd", "point": 2, "pris": 100, "inkomststigning": 10},
                                 {"tekst": "B. Tog", "point": 5, "pris": 12, "inkomststigning": 7},
                                 {"tekst": "C. Hus", "point": 100, "pris": 12, "inkomststigning": 10}],
              "svar": "A"
@@ -131,28 +131,31 @@ while running:
             # text on pygame
             penge_tekst = font.render(f"Penge: {penge},-", False, (250, 250, 0))
             inkomst_tekst = font.render(f"Indkomst: {inkomst},-", False, (250, 250, 0))
+            question_text = font.render(f"{quiz_set['spørgsmål']}", False, (250, 250, 0))
 
-            inkomst_tekst_rect = inkomst_tekst.get_rect()
             penge_tekst_rect = penge_tekst.get_rect()
+            inkomst_tekst_rect = inkomst_tekst.get_rect()
+            question_text_rect = question_text.get_rect()
 
             penge_tekst_rect.bottomright = (width, height)
             inkomst_tekst_rect.bottomleft = (0, height)
+            question_text_rect.center = (width//2, height//10)
 
             screen.blit(penge_tekst, penge_tekst_rect)
             screen.blit(inkomst_tekst, inkomst_tekst_rect)
-
-            # spørgsmål
-            question_text = font.render(f"{quiz_set['spørgsmål']}", False, (250, 250, 0))
-
-            question_text_rect = question_text.get_rect()
-
-            question_text_rect.center = (width//2, height//10)
             screen.blit(question_text, question_text_rect)
 
             print(f'\nPenge: {penge}, Inkomst: {inkomst} \n{quiz_set["spørgsmål"]}')
 
             # prints the options
+            pos = 0
             for i in quiz_set["valgmuligheder"]:
+                pos += 50
+                question_text = font.render(f"{i['tekst']} : {i['pris']},-", False, (250, 250, 0))
+                question_text_rect = penge_tekst.get_rect()
+                question_text_rect.topleft = (width//10, height//10 + pos)
+                screen.blit(question_text, question_text_rect)
+
                 print(f'{i["tekst"]} : {i["pris"]}kr')
 
             # it only stops when typed correct
