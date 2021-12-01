95ea9e1485f73b400122c10933e2ea869596b56a
Sofus Wenøe <sofuswenoee@gmail.com>
2021-12-01 19:36:10 +0100

    2 Bars at the right location. And have added prototype functions that connect points with bars.

--- pygame_stuff/progress_bar.py
+++ pygame_stuff/progress_bar.py
@@ -7,30 +7,43 @@ def update_bar(screen, point):
     # dette er bare en test for at se om det virker
     point_max = 100
     point_min = -100
+    x_unload = 270
+    y_unload = 0
+    x_load = 270
+    point = 100
+
+    if point_min >= point >= point_max():
+        y_load = 20 - point
+
+    elif point_min >= point:
+        y_load = 50
+
+    elif point_max <= point:
+        y_load = 0
 
     unloadedbar = pygame.image.load("Billeder/unloadedbar.png")
     loadedbar = pygame.image.load("Billeder/loadedbar.png")
     unloadedbar_rect = unloadedbar.get_rect()
     loadedbar_rect = loadedbar.get_rect()
 
-    loadedbar_rect.height = 200
-    loadedbar_rect.width = 100
-    loadedbar = pygame.transform.scale(loadedbar, (160, 320))
+    loadedbar_rect.height = 800
+    loadedbar_rect.width = 400
+    loadedbar = pygame.transform.scale(loadedbar, (400, 800))
     print(loadedbar.get_width())
 
-    unloadedbar_rect.height = 200
-    unloadedbar_rect.width = 100
-    unloadedbar = pygame.transform.scale(unloadedbar, (160, 320))
+    unloadedbar_rect.height = 800
+    unloadedbar_rect.width = 400
+    unloadedbar = pygame.transform.scale(unloadedbar, (400, 800))
     print(unloadedbar.get_width())
 
     # set det fra dette link: https://stackoverflow.com/questions/20842801/how-to-display-text-in-pygame
     pygame.font.init()
     myfont = pygame.font.SysFont('Comic Sans MS', 30)
     textsurface = myfont.render('Some Text', False, (200, 15, 10))
-    screen.blit(loadedbar, (0, 0))
-    screen.blit(unloadedbar, (0, 0))
+    screen.blit(unloadedbar, (x_unload, y_unload))
+    screen.blit(loadedbar, (x_load, y_load))
     pygame.display.update()
-    #if point_min >= point >= point_max():
+
 
 
     pygame.display.flip()
