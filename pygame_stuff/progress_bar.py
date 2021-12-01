5de07b5019165fdad5aa605deec280b1ec7ba7a7
Sofus Wenøe <sofuswenoee@gmail.com>
2021-12-01 13:56:57 +0100

    Able to see progress bar

--- pygame_stuff/progress_bar.py
+++ pygame_stuff/progress_bar.py
@@ -8,25 +8,26 @@ def update_bar(screen, point):
     point_max = 100
     point_min = -100
 
-    unloadedbar = pygame.image.load(
-        r"C:\Users\sofus\FIRE-game\FIRE-game\Billeder\unloadedbar.png").convert_alpha()
-    loadedbar = pygame.image.load(
-        r"C:\Users\sofus\FIRE-game\FIRE-game\Billeder\loadedbar.png").convert_alpha()
-
+    unloadedbar = pygame.image.load("Billeder/loadedbar.png")
+    loadedbar = pygame.image.load("Billeder/loadedbar.png")
     unloadedbar_rect = unloadedbar.get_rect()
     loadedbar_rect = loadedbar.get_rect()
 
     unloadedbar_rect.x = 100
     unloadedbar_rect.y = 200
 
-    loadedbar_rect.x = 100
-    loadedbar_rect.y = 200
+    loadedbar_rect.height = 200
+    loadedbar_rect.width = 100
+    loadedbar = pygame.transform.scale(loadedbar, (20, 40))
+    print(loadedbar.get_width())
+
 
     # set det fra dette link: https://stackoverflow.com/questions/20842801/how-to-display-text-in-pygame
     pygame.font.init()
     myfont = pygame.font.SysFont('Comic Sans MS', 30)
     textsurface = myfont.render('Some Text', False, (200, 15, 10))
-    screen.blit(textsurface, (0,0))
+    screen.blit(loadedbar, (0,0))
+    pygame.display.update()
     #if point_min >= point >= point_max():
 
 
